df7a5a33c015c59add2e6499f9d345b6c20c0088
nycbjr <nycbjr@gmail.com>
2013-08-25 15:57:09 -0400

    merging with cm

--- src/com/android/settings/cyanogenmod/QuickSettingsUtil.java
+++ src/com/android/settings/cyanogenmod/QuickSettingsUtil.java
@@ -169,13 +169,7 @@ public class QuickSettingsUtil {
         }
     }
 
-    private static boolean sUnsupportedRemoved = false;
-
     private static synchronized void removeUnsupportedTiles(Context context) {
-        if (sUnsupportedRemoved) {
-            return;
-        }
-
         // Don't show mobile data options if not supported
         if (!QSUtils.deviceSupportsMobileData(context)) {
             removeTile(TILE_MOBILEDATA);
@@ -207,8 +201,6 @@ public class QuickSettingsUtil {
         if (!QSUtils.deviceSupportsCamera()) {
             removeTile(TILE_CAMERA);
         }
-
-        sUnsupportedRemoved = true;
     }
 
     private static synchronized void refreshAvailableTiles(Context context) {
@@ -261,24 +253,27 @@ public class QuickSettingsUtil {
         return ENABLED_TILES.containsKey(id);
     }
 
-    public static String getCurrentTiles(Context context) {
+    public static String getCurrentTiles(Context context, boolean isRibbon) {
         String tiles = Settings.System.getString(context.getContentResolver(),
-                Settings.System.QUICK_SETTINGS_TILES);
+                isRibbon ? Settings.System.QUICK_SETTINGS_RIBBON_TILES
+                         : Settings.System.QUICK_SETTINGS_TILES);
         if (tiles == null) {
             tiles = getDefaultTiles(context);
         }
         return tiles;
     }
 
-    public static void saveCurrentTiles(Context context, String tiles) {
+    public static void saveCurrentTiles(Context context, String tiles, boolean isRibbon) {
         Settings.System.putString(context.getContentResolver(),
-                Settings.System.QUICK_SETTINGS_TILES, tiles);
+                isRibbon ? Settings.System.QUICK_SETTINGS_RIBBON_TILES
+                         : Settings.System.QUICK_SETTINGS_TILES, tiles);
     }
 
-    public static void resetTiles(Context context) {
+    public static void resetTiles(Context context, boolean isRibbon) {
         String defaultTiles = getDefaultTiles(context);
         Settings.System.putString(context.getContentResolver(),
-                Settings.System.QUICK_SETTINGS_TILES, defaultTiles);
+                isRibbon ? Settings.System.QUICK_SETTINGS_RIBBON_TILES
+                         : Settings.System.QUICK_SETTINGS_TILES, defaultTiles);
     }
 
     public static String mergeInNewTileString(String oldString, String newString) {
